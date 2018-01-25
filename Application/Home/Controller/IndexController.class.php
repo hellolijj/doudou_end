7be6e148b7d8e63aba464e2923fd4e7198b6ffd6
junjun.li <junjun.li@husor.com>
2018-01-26 00:56:34 +0800

    项目初始化

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+namespace Home\Controller;
+
+/**
+ * 前台首页控制器
+ * 主要获取首页聚合数据
+ */
+class IndexController extends BaseController {
+
+	//系统首页
+    public function index(){
+
+        $is_login = is_login();
+        echo json_encode($is_login);
+        echo 12;
+    }
+
+}
