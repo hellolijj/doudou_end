cc33cfb83458c2262664178e595dc9a1dbdfb063
Nobody <nobody@yq150.onlinetools>
2018-01-28 18:19:11 +0800

    git commit by online edit file 1/Application/Api/Controller/GatewayController.class.php

--- Application/Api/Controller/GatewayController.class.php
+++ Application/Api/Controller/GatewayController.class.php
@@ -39,6 +39,7 @@ class GatewayController extends BaseController {
         // 对方法的判断
         $result = new BaseLogic();
         if (!method_exists($logic, $function_name)) {
+            echo 1;die;
             $this->ajaxReturn($result->setError('无效的API参数'));
         }
         // 处理openid uid
