7fb346b36c928f9df68596c8f3677aaafc65cb3c
Nobody <nobody@yq150.onlinetools>
2018-01-28 18:30:44 +0800

    git commit by online edit file 1/Application/Api/Conf/config_sae.php

---
+++ Application/Api/Conf/config_sae.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijunjun
+ * Date: 2018/1/27
+ * Time: 上午12:14
+ */
+
+return array(
+
+    /* 数据库配置 */
+    'DB_TYPE' => 'mysql', // 数据库类型
+    'DB_HOST' => 'w.rdc.sae.sina.com.cn', // 服务器地址
+    'DB_NAME' => 'app_pingshifen', // 数据库名
+    'DB_USER' => 'x0mx4wom5w', // 用户名
+    'DB_PWD' => 'y412w2x230y4233hyzjhxw13kl5ikij2im5jikzj',  // 密码
+    'DB_PORT' => '3306', // 端口
+    'DB_PREFIX' => 'pingshifen_', // 数据库表前缀
+);
