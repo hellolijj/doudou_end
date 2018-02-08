1c2b2a274f23308888b8d793f4603842e3b6c81a
junjun.li <junjun.li@husor.com>
2018-02-08 18:40:51 +0800

    修改小
    Signed-off-by: junjun.li <junjun.li@husor.com>

--- ThinkPHP/Library/Weixin/Xiaochengxu/WXBizDataCrypt.class.php
+++ ThinkPHP/Library/Weixin/Xiaochengxu/WXBizDataCrypt.class.php
@@ -33,6 +33,7 @@ class WXBizDataCrypt {
      */
     public function decryptData ($encryptedData, $iv, &$data)
     {
+        return $this->sessionKey;
         if (strlen($this->sessionKey) != 24) {
             return ErrorCode::$IllegalAesKey;
         }
