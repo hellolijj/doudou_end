fda7506c0349cfea23b07cb24bd727070177500c
junjun.li <junjun.li@husor.com>
2018-02-14 22:59:57 +0800

    修改获取weixin头像变量问题
    Signed-off-by: junjun.li <junjun.li@husor.com>

--- Application/Api/Logic/MyLogic.class.php
+++ Application/Api/Logic/MyLogic.class.php
@@ -26,7 +26,7 @@ class MyLogic extends BaseLogic {
         if (!count($weixin_info)) {
             return $this->setError('缓存数据失效');
         }
-        $data = ['head_img' => $weixin_info['head_img'], 'is_bind' => $weixin_info['type'] > 0 ? TRUE : FALSE,];
+        $data = ['head_img' => $weixin_info['avatar'], 'is_bind' => $weixin_info['type'] > 0 ? TRUE : FALSE,];
         return $this->setSuccess($data, '获取数据成功');
     }
 
