f11e999f55cb217ddfbb264b702582326c8a152f
junjun.li <junjun.li@husor.com>
2018-02-19 12:04:52 +0800

    更改数据库配置
    Signed-off-by: junjun.li <junjun.li@husor.com>

--- Application/Api/Conf/config_sae.php
+++ Application/Api/Conf/config_sae.php
@@ -9,11 +9,12 @@
 return array(
 
     /* 数据库配置 */
+    'DB_DEPLOY_TYPE' => 1, // 设置分布式数据库支持
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'w.rdc.sae.sina.com.cn', // 服务器地址
+    'DB_HOST' => 'w.rdc.sae.sina.com.cn,r.rdc.sae.sina.com.cn', // 服务器地址
     'DB_NAME' => 'app_pingshifen', // 数据库名
-    'DB_USER' => 'x0mx4wom5w', // 用户名
-    'DB_PWD' => 'y412w2x230y4233hyzjhxw13kl5ikij2im5jikzj',  // 密码
+    'DB_USER' => 'zwmzjymjk1', // 用户名
+    'DB_PWD' => 'km3wi1h2w0j5k4xiw1xh5hiz5l1514ik0l3ihl1x',  // 密码
     'DB_PORT' => '3306', // 端口
     'DB_PREFIX' => 'pingshifen_', // 数据库表前缀
 
