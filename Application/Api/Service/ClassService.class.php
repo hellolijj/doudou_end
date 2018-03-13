e71ec70f0888c8827fd4febd320c906f27126c3a
junjun.li <junjun.li@husor.com>
2018-03-13 21:50:40 +0800

    还原修改内容

--- Application/Api/Service/ClassService.class.php
+++ Application/Api/Service/ClassService.class.php
@@ -60,6 +60,9 @@ class ClassService extends BaseService {
         }
         $course_ids = result_to_array($classes, 'cid');
         $courses = D('Course')->getCourseByUids($course_ids, $page, $page_size);
+        if (!$courses) {
+            return ['success' => FALSE, 'message' => '无锁定课程'];
+        }
         $tids = result_to_array($courses, 'uid');
         $teachers = D('Teacher')->getByIds($tids);
         $teachers_result = result_to_map($teachers, 'id');
