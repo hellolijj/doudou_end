8cefa3d892072891bb919c76ea9369593fce7805
junjun.li <junjun.li@husor.com>
2018-03-20 20:55:42 +0800

    换用 redirect 函数

--- Application/Page/Controller/IndexController.class.php
+++ Application/Page/Controller/IndexController.class.php
@@ -12,7 +12,8 @@ class IndexController extends Controller
 
     // 使用帮助页面
     public function help() {
-        $contents_url = 'https://mp.weixin.qq.com/s/afxA7ZvpI_VFQUrhaZuC9Q';
-        echo file_get_contents($contents_url);
+        $contents_url = 'https://mp.weixin.qq.com/s/F9X9NykRfKKA9uZPJEnj9g';
+//        echo file_get_contents($contents_url, TRUE);
+        redirect($contents_url);
     }
 }
