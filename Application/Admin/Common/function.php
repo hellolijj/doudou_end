1f2763805dca1a793370eb38e061cd03d9ac0f86
junjun.li <junjun.li@husor.com>
2018-03-26 23:57:38 +0800

    add ssl

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -12,8 +12,9 @@ function email() {
     }
 
     $mail->clean(); //重用此对象
-    $ret = $mail->quickSend("1217046214@qq.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj", "smtp.sina.com", 25); //指定smtp和端口
+    $ret = $mail->quickSend("1217046214@qq.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj", "smtp.sina.com", 25, TRUE); //指定smtp和端口
 
+//    'zvalkqmewgnyjfeh'
     //发送失败时输出错误码和错误信息
     if ($ret === false) {
         var_dump($mail->errno(), $mail->errmsg());
