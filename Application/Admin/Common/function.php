e6b4405b50b3785cd1c749f53a66fde5b305e639
junjun.li <junjun.li@husor.com>
2018-03-26 23:46:16 +0800

    update email smtp again

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -1,20 +1,24 @@
 <?php
 
 function email() {
-    $mail = new SaeMail();
-    $ret = $mail->quickSend( '1217046214@qq.com', //接受者邮箱
-        '邮件标题',   //邮件标题
-        '邮件发送成功',   //邮件内容
-        'hello_lijj@qq.com' , //发送方邮箱
-        'hello_lijj',             //发送方邮箱密码
-        'smtp.qq.com',
-        465);
+
+    $mail = new \SaeMail();
+
+    $ret = $mail->quickSend("1217046214@qq.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj");
 
     //发送失败时输出错误码和错误信息
-    if ($ret === false)
+    if ($ret === false) {
         var_dump($mail->errno(), $mail->errmsg());
-    else
-        echo '发生成功';
+    }
 
+    $mail->clean(); //重用此对象
+    $ret = $mail->quickSend("1217046214@qq.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj", "smtp.sina.com", 25); //指定smtp和端口
+
+    //发送失败时输出错误码和错误信息
+    if ($ret === false) {
+        var_dump($mail->errno(), $mail->errmsg());
+    }
+
+    echo '发送成功';
 
 }
