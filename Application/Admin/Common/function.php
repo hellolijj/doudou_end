139ec9501147f4f17bf6a9f932a7f0f7464ca789
junjun.li <junjun.li@husor.com>
2018-03-27 00:04:28 +0800

    再测试一会

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -4,15 +4,7 @@ function email() {
 
     $mail = new \SaeMail();
 
-    $ret = $mail->quickSend("1217046214@qq.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj");
-
-    //发送失败时输出错误码和错误信息
-    if ($ret === false) {
-        var_dump($mail->errno(), $mail->errmsg());
-    }
-
-    $mail->clean(); //重用此对象
-    $ret = $mail->quickSend("1217046214@qq.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj", "smtp.sina.com", 25, TRUE); //指定smtp和端口
+    $ret = $mail->quickSend("hello_lijj@foxmail.com", "邮件标题", "邮件内容", "hello_lijj@qq.com", "hello_lijj", "smtp.sina.com", 25, TRUE); //指定smtp和端口
 
 //    'zvalkqmewgnyjfeh'
     //发送失败时输出错误码和错误信息
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -12,15 +12,7 @@ class IndexController extends Controller {
     public function email() {
         $mail = new \SaeMail();
 
-        $ret = $mail->quickSend("hello_lijj@foxmail.com", "邮件标题", "邮件内容", "hello_lijj@sina.com", "hello_lijj");
-
-        //发送失败时输出错误码和错误信息
-        if ($ret === false) {
-            var_dump($mail->errno(), $mail->errmsg());
-        }
-
-        $mail->clean(); //重用此对象
-        $ret = $mail->quickSend("hello_lijj@foxmail.com", "邮件标题", "邮件内容", "hello_lijj@sina.com", "hello_lijj", "smtp.sina.com", 25); //指定smtp和端口
+        $ret = $mail->quickSend("hello_lijj@qq.com", "邮件标题", "邮件内容", "hello_lijj@sina.com", "hello_lijj", "smtp.sina.com", 25); //指定smtp和端口
 
         //发送失败时输出错误码和错误信息
         if ($ret === false) {
