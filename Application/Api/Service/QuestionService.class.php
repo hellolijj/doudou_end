05d0d3d71698925c84eb8204626904777fe2ffea
junjun.li <junjun.li@husor.com>
2018-04-01 16:12:21 +0800

    再测试一会

--- Application/Api/Service/QuestionService.class.php
+++ Application/Api/Service/QuestionService.class.php
@@ -58,6 +58,7 @@ class QuestionService extends BaseService {
 
         $question_count_items = D('QuestionCount')->getByQids($question_id_arr);
         $question_count_items = result_to_map($question_count_items, 'qid');
+        print_r($question_count_items);die;
 
 
         foreach ($question_items as &$question_item) {
