a2e058c53b146245be97fd8b35d3bc6ab2bb32f7
junjun.li <junjun.li@husor.com>
2018-06-17 07:58:27 +0800

    修改数据库配置并暂停应用

--- Application/Api/Conf/config_sae.php
+++ Application/Api/Conf/config_sae.php
@@ -11,14 +11,21 @@ return array(
     /* 数据库配置 */
     'DB_DEPLOY_TYPE' => 1, // 设置分布式数据库支持
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'w.rdc.sae.sina.com.cn,r.rdc.sae.sina.com.cn', // 服务器地址
+
+//    'DB_HOST' => 'w.rdc.sae.sina.com.cn,r.rdc.sae.sina.com.cn', // 服务器地址
+//    'DB_NAME' => 'app_pingshif', // 数据库名
+//    'DB_USER' => 'zwmzjymjk1', // 用户名
+//    'DB_PWD' => 'km3wi1h2w0j5k4xiw1xh5hiz5l1514ik0l3ihl1x',  // 密码
+//    'DB_PORT' => '3306', // 端口
+
+
+    'DB_HOST' => 'kbvaybcarhcv.mysql.sae.sina.com.cn,lgobeqojbmjb.mysql.sae.sina.com.cn', // 服务器地址
+    'DB_PORT' => '10059',
     'DB_NAME' => 'app_pingshif', // 数据库名
-    'DB_USER' => 'zwmzjymjk1', // 用户名
-    'DB_PWD' => 'km3wi1h2w0j5k4xiw1xh5hiz5l1514ik0l3ihl1x',  // 密码
-    'DB_PORT' => '3306', // 端口
+    'DB_USER' => 'admin', // 用户名
+    'DB_PWD' => '123456',  // 密码
+
     'DB_PREFIX' => 'pingshifen_', // 数据库表前缀
-
-
     /* 文件上传路径 */
     'APP_ROOT' => 'https://pingshifen.applinzi.com/', 'UPLOAD_DIR' => 'img/', 'UPLOAD_ROOT' => 'http://pingshif-img.stor.sinaapp.com/',
 
