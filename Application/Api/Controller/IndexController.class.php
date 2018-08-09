142dff7e3cc966e52affdb696ff2480964765298
李俊君 <hello_lijj@qq.com>
2018-08-09 17:56:14 +0800

    爬虫明星数据

--- Application/Api/Controller/IndexController.class.php
+++ Application/Api/Controller/IndexController.class.php
@@ -22,4 +22,53 @@ class IndexController extends BaseController {
 
     }
 
+
+    // 爬取排名明星粉丝数据
+    public function craw() {
+        $reqi_url = 'http://node.video.qq.com/x/api/doki_rank_detail?starids=150054,1320228&fantuans=150054,1320228';
+        $reqi_contents = file_get_contents($reqi_url);
+        $reqi_contents = json_decode($reqi_contents, TRUE);
+        $yangyunqing_value = intval($reqi_contents[150054]['stFanTuanScoreInfo']['ddwPopularity']);
+        $xiaozhan_value = intval($reqi_contents[1320228]['stFanTuanScoreInfo']['ddwPopularity']);
+
+
+        return ['sunnee' => $yangyunqing_value, 'xz' => $xiaozhan_value];
+    }
+
+    public function inserdb() {
+
+        $value = $this->craw();
+        $xz_value = $value['xz'];
+        $sunnee_value = $value['sunnee'];
+        if (!$xz_value) {
+            $xz_value = 0;
+        }
+        if (!$sunnee_value) {
+            $sunnee_value = 0;
+        }
+
+        $xz_last_value = 0;
+        $sunnee_last_value = 0;
+
+        $last_value = M('crew_mingxing')->order('id desc')->limit(1)->select();
+
+
+        if ($last_value) {
+            $xz_last_value = $last_value[0]['xz'];
+            $sunnee_last_value = $last_value[0]['sunnee'];
+        }
+
+        $data = [
+            'time' => date('md H:m', time()),
+            'xz' => $xz_value,
+            'sunnee' => $sunnee_value,
+            'gap' => $sunnee_value -  $xz_value,
+            'xz_incre' => $xz_value - $xz_last_value,
+            'sunnee_incre' => $sunnee_value - $sunnee_last_value,
+            'gmt_create' => time(),
+        ];
+        M('crew_mingxing')->add($data);
+
+    }
+
 }
